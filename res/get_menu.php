b0064067c73687fe50e18077ec2278309ead48e3
kans-th0 <kans-th0@klth147846.net.lth.se>
2015-09-01 09:55:00 +0200

    Merge origin/master
    Conflicts:
    	classes/class.get_menu_functions.php
    	res/get_menu.php

--- res/get_menu.php
+++ res/get_menu.php
@@ -172,7 +172,7 @@ ON node.lft BETWEEN parent.lft AND parent.rgt
     function makeNested($source, $nested) {
         try {
             foreach ( $source as &$s ) {
-                if ( $s['node_pid']==0 || ($s['node_pid']=$s['node_uid']) ) {
+                if ( $s['node_pid']==0 || ($s['node_pid']===$s['node_uid']) ) {
                     // no parent_id so we put it in the root of the array
                     $nested[] = &$s;
                 } else {
