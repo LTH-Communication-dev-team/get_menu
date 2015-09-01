03489fe0677c94f5d9a15fd6117e2123a893f5ce
kans-th0 <kans-th0@klth147846.net.lth.se>
2015-09-01 21:10:54 +0200

    Merge origin/master
    Conflicts:
    	classes/class.get_menu_functions.php
    	res/get_menu.php

--- tasks/class.tx_getmenu_tree_transformer.php
+++ tasks/class.tx_getmenu_tree_transformer.php
@@ -11,24 +11,23 @@
 	
 	public function traverse($i_id)
         {
+            //echo $i_id . '; ';
             $i_lft = $this->i_count;
             $this->i_count++;
 
             $a_kid = $this->get_children($i_id);
 
-            if ($a_kid) 
-            {
-                foreach($a_kid as $a_child) 
-                {
+            if ($a_kid) {
+                foreach($a_kid as $a_child) {
                     $this->traverse($a_child);
                 }
             }
-            $i_rgt=$this->i_count;
+            $i_rgt = $this->i_count;
             $this->i_count++;
-            $this->write($i_lft,$i_rgt,$i_id);
+            $this->write($i_lft, $i_rgt, $i_id);
         }   
 	
-	function execute($input_uid=0)
+	function execute($input_uid = 0)
 	{
             //set_time_limit(0);
 	    $executionSucceeded = FALSE;
@@ -37,29 +36,37 @@
 
 	    // build a complete copy of the adjacency table in ram
             $unix_timestamp = time();
-	    $s_query = "SELECT uid,pid FROM pages 
+	    /*$s_query = "SELECT uid,pid FROM pages 
                 WHERE deleted = 0 AND NOT t3ver_state>0 AND doktype<199
-                ORDER BY sorting";
-	    $i_result = mysql_query($s_query);
-	    $a_rows = array();
-	    while ($a_rows[] = mysql_fetch_assoc($i_result));
-	    $a_link = array();
-
-	    foreach($a_rows as $a_row) 
-	    {
-		$i_father_id = $a_row['pid'];
-		$i_child_id = $a_row['uid'];
-		if (!array_key_exists($i_father_id,$a_link)) 
-		{
-		    $a_link[$i_father_id]=array();
+                ORDER BY sorting";*/
+	    //$i_result = mysql_query($s_query);
+	    //$a_rows = array();
+            $a_link = array();
+	    //while ($a_rows[] = mysql_fetch_assoc($i_result));*/
+            $res = $GLOBALS['TYPO3_DB']->exec_SELECTquery('uid, pid', 'pages', 'deleted = 0 AND hidden = 0 AND NOT t3ver_state>0', '', 'sorting');
+            while ($row = $GLOBALS["TYPO3_DB"]->sql_fetch_assoc($res)) {
+                //$a_rows[] = $row;
+                $i_father_id = $row['pid'];
+		$i_child_id = $row['uid'];
+		if (!array_key_exists($i_father_id, $a_link)) {
+		    $a_link[$i_father_id] = array();
 		}
-		$a_link[$i_father_id][]=$i_child_id;
-	    }
+		$a_link[$i_father_id][] = $i_child_id;
+            }
+            $GLOBALS['TYPO3_DB']->sql_free_result($res);
 	    
-	    if(!is_array($a_link)) throw new Exception("First parameter should be an array. Instead, it was type '".gettype($a_link)."'");
+
+	   /* foreach($a_rows as $a_row) 
+	    {
+		
+	    }*/
+	    
+	    if(!is_array($a_link)) {
+                throw new Exception("First parameter should be an array. Instead, it was type '".gettype($a_link)."'");
+            }
             $this->i_count = 1;
-            $this->a_link= $a_link;
-	    
+            $this->a_link = $a_link;
+	    //print_r($this->a_link);
 	    //$o_tree_transformer = new tree_transformer($a_link);
 	    $this->traverse(0);
             
@@ -70,6 +77,7 @@
 	    return $executionSucceeded;
         }   
 
+        
         private function get_children($i_id) 
         {
 	    if ( ! isset($this->a_link[$i_id])) {
@@ -79,13 +87,19 @@
             return $this->a_link[$i_id];
         }
 
-        private function write($i_lft,$i_rgt,$i_id) 
+        
+        private function write($i_lft, $i_rgt, $i_id) 
         {
+            //echo "$i_lft,$i_rgt,$i_id;";
             // fetch the source column
 	    $unix_timestamp = time();
-            $s_query = "SELECT * FROM pages WHERE uid=$i_id";
+            //$s_query = "SELECT * FROM pages WHERE uid=$i_id";
+            /*$a_source = array();
+            $res = $GLOBALS['TYPO3_DB']->exec_SELECTquery('*', 'pages', 'uid='. $i_id);
+            $a_source = $GLOBALS["TYPO3_DB"]->sql_fetch_assoc($res);*/
+                
 
-            if (!$i_result = mysql_query($s_query))
+            /*if (!$i_result = mysql_query($s_query))
             {
                 echo "<pre>$s_query</pre>\n";
                 throw new Exception(mysql_error());  
@@ -94,13 +108,13 @@
             if (mysql_num_rows($i_result))
             {
                 $a_source = mysql_fetch_assoc($i_result);
-            }
+            }*/
 
             // root node?  label it unless already labeled in source table
-            if (1 == $i_lft && empty($a_source['title']))
+            /*if (1 == $i_lft && empty($a_source['title']))
             {
                 $a_source['title'] = 'ROOT';
-            }
+            }*/
 
             // insert into the new nested tree table
             // use mysql_real_escape_string because one value "CD's"  has a single '
@@ -114,21 +128,24 @@
                     '".mysql_real_escape_string($a_source['title'])."'
                 )
             ";*/
-	    $u_query = "UPDATE pages SET lft = $i_lft,rgt=$i_rgt WHERE uid = $i_id";
+	    //$u_query = "UPDATE pages SET lft = $i_lft,rgt=$i_rgt WHERE uid = $i_id";
 	    try {
-		$i_result = mysql_query($u_query);
-                $GLOBALS['TYPO3_DB']->sql_free_result($i_result);
+                //echo $i_id . '; ';
+                $GLOBALS['TYPO3_DB']->exec_UPDATEquery('pages', 'uid='.intval($i_id), array('lft' => intval($i_lft), 'rgt' => intval($i_rgt), 'tstamp' => $unix_timestamp));
+		
+                //$i_result = mysql_query($u_query);
+                //$GLOBALS['TYPO3_DB']->sql_free_result($i_result);
 	    } catch(Exception $e) {
-
+                die($e);
 	    }
         }
         
         function addRootId()
         {
-            $res = $GLOBALS['TYPO3_DB']->exec_SELECTquery('uid', 'pages', 'deleted=0 AND NOT t3ver_state>0 AND doktype<200','','sorting');
+            $res = $GLOBALS['TYPO3_DB']->exec_SELECTquery('uid', 'pages', 'deleted=0 AND hidden = 0 AND NOT t3ver_state > 0', '', 'sorting');
             while ($row = $GLOBALS["TYPO3_DB"]->sql_fetch_assoc($res)) {
                 $rootid = $this->getRoot($row['uid']);
-                if($rootid) {
+                if($rootid!==false) {
                     $GLOBALS['TYPO3_DB']->exec_UPDATEquery('pages', 'uid='.$row['uid'], array('root' => $rootid, 'tstamp' => time()));
                 } else {
                     $GLOBALS['TYPO3_DB']->exec_UPDATEquery('pages', 'uid='.$row['uid'], array('root' => 0, 'tstamp' => time()));
@@ -139,14 +156,18 @@
         
         function getRoot($uid)
         {
-            $sql = "SELECT SUBSTRING_INDEX(GROUP_CONCAT(template.pid),',',-1) AS rootid
+            /*$sql = "SELECT SUBSTRING_INDEX(GROUP_CONCAT(template.pid),',',-1) AS rootid
             FROM pages AS node
             JOIN pages AS parent ON node.lft BETWEEN parent.lft AND parent.rgt AND parent.deleted = 0
             LEFT JOIN sys_template template ON parent.uid=template.pid AND template.root=1
             WHERE node.uid = $uid
-            ORDER BY node.lft";
-            $res1 = $GLOBALS['TYPO3_DB']->sql_query($sql);
+            ORDER BY node.lft";*/
+            $res1 = $GLOBALS['TYPO3_DB']->exec_SELECTquery("SUBSTRING_INDEX(GROUP_CONCAT(template.pid),',',-1) AS rootid", 
+                    "pages AS node JOIN pages AS parent ON node.lft BETWEEN parent.lft AND parent.rgt AND parent.deleted = 0 LEFT JOIN sys_template template ON parent.uid=template.pid AND template.root=1", 
+                    "node.uid = " . $uid, "", "node.lft");
+            //$res1 = $GLOBALS['TYPO3_DB']->sql_query($sql);
             $row1 = $GLOBALS["TYPO3_DB"]->sql_fetch_assoc($res1);
+            //Secho $row1['rootid'] . '; ';
             if(isset($row1['rootid'])){
                 return $row1['rootid'];
             } else {
