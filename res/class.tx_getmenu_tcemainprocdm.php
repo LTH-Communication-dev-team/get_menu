708936b546e4ad87a9fc173baccfe21c95d73c3c
kans-th0 <kans-th0@klth147846.net.lth.se>
2015-10-27 10:55:10 +0100

    Merge origin/master
    Conflicts:
    	classes/class.get_menu_functions.php
    	res/get_menu.php

--- res/class.tx_getmenu_tcemainprocdm.php
+++ res/class.tx_getmenu_tcemainprocdm.php
@@ -42,7 +42,7 @@ class tx_getmenu_tcemainprocdm {
     function clearCachePostProc($_params, $pObj)
     {
 	//var_dump($pObj);
-        //$GLOBALS['TYPO3_DB']->exec_INSERTquery('tx_devlog', array('msg' => '45' . $_params['cacheCmd']. ','.$_params['table']. ','.$_params['uid_page'], 'crdate' => time()));
+        $GLOBALS['TYPO3_DB']->exec_INSERTquery('tx_devlog', array('msg' => '45' . $_params['cacheCmd']. ','.$_params['table']. ','.$_params['uid_page'], 'crdate' => time()));
 
         $uid_page = $_params['uid_page'];
         $get_menuObj = new get_menu_functions;
