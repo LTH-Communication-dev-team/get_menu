967af02cc669eb93c12c04189c08c2d55430d7a8
kans-th0 <kans-th0@klth147846.net.lth.se>
2015-11-09 13:18:46 +0100

    Merge origin/master
    Conflicts:
    	classes/class.get_menu_functions.php
    	res/get_menu.php

--- classes/class.get_menu_functions.php
+++ classes/class.get_menu_functions.php
@@ -92,7 +92,7 @@ class get_menu_functions {
             }
             //Clear varnish cache
             if($domainName) {
-                $wholePath = str_replace('//','/', trim($domainName).'/'.$pagePath.'sucker!');
+                $wholePath = str_replace('//','/', trim($domainName).'/'.$pagePath);
                 $this->ban('http://' . $wholePath);
             }
             echo $wholePath;
